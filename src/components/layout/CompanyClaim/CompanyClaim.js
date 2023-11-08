4bb06ad90e8fcbb3174d99ca3451c75fb00e8bbb
Robert Synowiec <rsynowiec81@gmail.com>
2023-11-08 13:35:30 +0100

    Add maximum range of displayed numbers and changed font size in ShoppingBasket

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -6,34 +6,41 @@ import styles from './CompanyClaim.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 
-const CompanyClaim = () => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className='row align-items-center'>
-        <div className={`col text-left ${styles.phoneNumber}`}>
-          <p>
-            <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
-            222
-          </p>
-        </div>
-        <div className='col text-center'>
-          <a href='#'>
-            <img src='/images/logo.png' alt='Bazar' />
-          </a>
-        </div>
-        <div className={`col text-right ${styles.cart}`}>
-          <a href='#' className={styles.cartBox}>
-            <div className={styles.cartIcon}>
-              <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
-            </div>
-            <div className={styles.cartCounter}>0</div>
-          </a>
+const CompanyClaim = () => {
+  const number = 0;
+  const isValidNumber = number >= 0 && number <= 99999;
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row align-items-center'>
+          <div className={`col text-left ${styles.phoneNumber}`}>
+            <p>
+              <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560
+              - 222
+            </p>
+          </div>
+          <div className='col text-center'>
+            <a href='#'>
+              <img src='/images/logo.png' alt='Bazar' />
+            </a>
+          </div>
+          <div className={`col text-right ${styles.cart}`}>
+            <a href='#' className={styles.cartBox}>
+              <div className={styles.cartIcon}>
+                <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
+              </div>
+              <div className={styles.cartCounter}>
+                {' '}
+                {isValidNumber && <div>{number}</div>}
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 // CompanyClaim.propTypes = {};
 
 export default CompanyClaim;
