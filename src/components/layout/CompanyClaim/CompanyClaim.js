c59fa029b55e908067f4b9d8e73e0fe0270d5139
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-11-09 05:32:30 +0000

    TopBar & CompanyClaim components -> RWD

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -9,14 +9,16 @@ import { faMobileAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons
 const CompanyClaim = () => (
   <div className={styles.root}>
     <div className='container'>
-      <div className='row align-items-center'>
+      {/* FOR DESKTOP */}
+
+      <div className={`row align-items-center ${styles.desktop}`}>
         <div className={`col text-left ${styles.phoneNumber}`}>
           <p>
             <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560 -
             222
           </p>
         </div>
-        <div className='col text-center'>
+        <div className={`col text-center ${styles.logoBazar}`}>
           <a href='#'>
             <img src='/images/logo.png' alt='Bazar' />
           </a>
@@ -28,6 +30,32 @@ const CompanyClaim = () => (
             </div>
             <div className={styles.cartCounter}>0</div>
           </a>
+        </div>
+      </div>
+
+      {/* FOR MOBILE */}
+
+      <div className={`row align-items-center ${styles.mobile}`}>
+        <div className={`col text-center ${styles.logoBazar}`}>
+          <a href='#'>
+            <img src='/images/logo.png' alt='Bazar' />
+          </a>
+        </div>
+        <div className={`col mt-3`}>
+          <div className={`col text-center ${styles.cart}`}>
+            <a href='#' className={styles.cartBox}>
+              <div className={styles.cartIcon}>
+                <FontAwesomeIcon className={styles.icon} icon={faShoppingBasket} />
+              </div>
+              <div className={styles.cartCounter}>0</div>
+            </a>
+          </div>
+          <div className={`col text-center ${styles.phoneNumber}`}>
+            <p>
+              <FontAwesomeIcon className={styles.icon} icon={faMobileAlt} /> 2300 - 3560
+              - 222
+            </p>
+          </div>
         </div>
       </div>
     </div>
