12a69473fda531c970ccfe7129216e648eebdf11
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-11-11 17:09:05 +0300

    add items to initialState for categories chair and sofa and fix Product.box.test.js

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { addToFavorites, removeFromFavorites } from '../../../redux/productsRedux';
 import styles from './ProductBox.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -10,12 +11,32 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
+import initialState from '../../../redux/initialState';
 
-const ProductBox = ({ name, price, promo, stars }) => {
+const ProductBox = ({ id, name, price, promo, stars, category }) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector(state => state.products.favorites || {});
+  //console.log('Favorites:', favorites);
+
+  useEffect(() => {
+    if (Object.keys(favorites).length === 0) {
+      dispatch(addToFavorites({ id: initialState.products[0].id }));
+    }
+  }, [dispatch, favorites, id]);
+
+  const isFavorite = favorites[id];
+
+  const handleToggleFavorite = () => {
+    if (isFavorite) {
+      dispatch(removeFromFavorites({ id }));
+    } else {
+      dispatch(addToFavorites({ id }));
+    }
+  };
   return (
     <div className={styles.root}>
       <div className={styles.photo}>
-        <img src={`images/beds/${name}.jpg`} alt={name} />
+        <img src={`images/${category}s/${name}.jpg`} alt={name} />
         {promo && <div className={styles.sale}>{promo}</div>}
         <div className={styles.buttons}>
           <Button variant='small'>Quick View</Button>
@@ -41,7 +62,11 @@ const ProductBox = ({ name, price, promo, stars }) => {
       <div className={styles.line}></div>
       <div className={styles.actions}>
         <div className={styles.outlines}>
-          <Button variant='outline'>
+          <Button
+            variant='outline'
+            onClick={handleToggleFavorite}
+            className={isFavorite ? styles.favorite : ''}
+          >
             <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
           </Button>
           <Button variant='outline'>
@@ -57,12 +82,15 @@ const ProductBox = ({ name, price, promo, stars }) => {
     </div>
   );
 };
+
 ProductBox.propTypes = {
   children: PropTypes.node,
+  id: PropTypes.string,
   name: PropTypes.string,
   price: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
+  category: PropTypes.string,
 };
 
 export default ProductBox;
