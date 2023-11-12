b762b75ff56f6136ae3d748545537c57452c17cb
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-11-12 14:35:36 +0000

    Created Product banner for Desktops

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -2,7 +2,20 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 
-const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
+const ProductPage = () => (
+  <div className={styles.root}>
+    <div className='container'>
+      <div className={styles.banner}>
+        <h1 className={styles.bannerTitle}>
+          Bedroom <span className='font-weight-bold'>Furniture</span>
+        </h1>
+        <h3 className={styles.bannerSubtitle}>
+          Always <span className={styles.subtitleColor}>25%</span>off or more
+        </h3>
+      </div>
+    </div>
+  </div>
+);
 
 // ProductPage.propTypes = {};
 
