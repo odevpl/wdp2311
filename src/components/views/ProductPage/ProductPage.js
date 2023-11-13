5ef4097f696c6f94aadb54053c2f7a803292a382
Piotr Zientara <riozshow@gmail.com>
2023-11-13 23:27:17 +0100

    Create Reviews

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,8 +1,16 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
+import Reviews from '../../features/Reviews/Reviews';
 
-const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
+const ProductPage = () => (
+  <div className={styles.root}>
+    <div className='container'>
+      This is ProductPage
+      <Reviews />
+    </div>
+  </div>
+);
 
 // ProductPage.propTypes = {};
 
