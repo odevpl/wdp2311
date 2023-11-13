d1daba228a4a3ef0cb8b7a8a94ad606926736c7b
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-11-13 23:27:47 +0300

    solve conflicts in Promotional and Homepage

--- src/components/features/Promotional/Promotional.js
+++ src/components/features/Promotional/Promotional.js
@@ -2,38 +2,44 @@ import React, { useState } from 'react';
 import styles from './Promotional.module.scss';
 import { useSelector } from 'react-redux';
 import { allPromotional } from '../../../redux/productsRedux';
-import PromotionalProduct from '../../common/PromotionalProduct/PromotionalProduct';
-import BestDeal from '../BestDeal/BestDeal';
+import PromotionalProduct from '../PromotionalProduct/PromotionalProduct';
 
 const Promotional = () => {
   const [deal, setDeal] = useState(0);
-  const promotionalProducts = useSelector(allPromotional);
+  const promotionalProducts = Object.values(useSelector(allPromotional));
+  const handleDealChange = number => {
+    setDeal(number);
+  };
+
   return (
     <div className={styles.root}>
       <div className='container'>
         <div className='row'>
-          <div className={'col-4 ' + styles.hotDeal}>
-            <div className={styles.dealBg}>
+          <div className='col-5'>
+            <div className={'' + styles.dealBg}>
               <h3 className={styles.title}>hot deals</h3>
               <div className={'mr-3 ' + styles.dots}>
                 <ul>
                   {[0, 1, 2].map(i => (
                     <li key={i}>
-                      <a className={i === deal ? styles.active : ''}>view {i}</a>
+                      <a
+                        onClick={() => handleDealChange(i)}
+                        className={i === deal ? styles.active : ''}
+                      >
+                        view {i}
+                      </a>
                     </li>
                   ))}
                 </ul>
               </div>
             </div>
-            {promotionalProducts.map(item => (
-              <div key={item.id}>
+            {promotionalProducts.map((item, index) => (
+              <div key={item.id} className={index !== deal && 'd-none'}>
                 <PromotionalProduct {...item} />
               </div>
             ))}
           </div>
-          <div className='col-8'>
-            <BestDeal />
-          </div>
+          <div className='col-7'></div>
         </div>
       </div>
     </div>
---
+++ src/components/features/PromotionalProduct/PromotionalProduct.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './PromotionalProduct.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faStar,
+  faExchangeAlt,
+  faShoppingBasket,
+  faEye,
+} from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
+import Button from '../../common/Button/Button';
+
+const PromotionalProduct = ({ name, price, stars }) => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.photo}>
+        <img src={`images/beds/${name}.jpg`} alt={name} />
+        <div className={styles.buttons}>
+          <Button variant='small'>
+            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
+          </Button>
+        </div>
+      </div>
+      <div className={styles.content}>
+        <h5>{name}</h5>
+        <div className={styles.stars}>
+          {[1, 2, 3, 4, 5].map(i => (
+            <a key={i} href='#'>
+              {i <= stars ? (
+                <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+              ) : (
+                <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+              )}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.actions}>
+        <div className={styles.outlines}>
+          <Button variant='outline'>
+            <FontAwesomeIcon icon={faEye}>Add to compare</FontAwesomeIcon>
+          </Button>
+          <Button variant='outline'>
+            <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+          </Button>
+          <Button variant='outline'>
+            <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+          </Button>
+        </div>
+        <div className={styles.price}>
+          <Button noHover variant='small'>
+            $ {price}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+PromotionalProduct.propTypes = {
+  children: PropTypes.node,
+  name: PropTypes.string,
+  price: PropTypes.number,
+  promo: PropTypes.string,
+  stars: PropTypes.number,
+};
+
+export default PromotionalProduct;
