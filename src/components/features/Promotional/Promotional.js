2a034b261d76085d0e7c1ea20eb3c91392d500c6
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-11-13 23:31:36 +0300

    accept changes in StarsRating.scss,  useLayoutListener

--- src/components/features/Promotional/Promotional.js
+++ src/components/features/Promotional/Promotional.js
@@ -2,44 +2,39 @@ import React, { useState } from 'react';
 import styles from './Promotional.module.scss';
 import { useSelector } from 'react-redux';
 import { allPromotional } from '../../../redux/productsRedux';
-import PromotionalProduct from '../PromotionalProduct/PromotionalProduct';
+import PromotionalProduct from '../../common/PromotionalProduct/PromotionalProduct';
+import BestDeal from '../BestDeal/BestDeal';
 
 const Promotional = () => {
   const [deal, setDeal] = useState(0);
   const promotionalProducts = Object.values(useSelector(allPromotional));
-  const handleDealChange = number => {
-    setDeal(number);
-  };
 
   return (
     <div className={styles.root}>
       <div className='container'>
         <div className='row'>
-          <div className='col-5'>
-            <div className={'' + styles.dealBg}>
+          <div className={'col-4 ' + styles.hotDeal}>
+            <div className={styles.dealBg}>
               <h3 className={styles.title}>hot deals</h3>
               <div className={'mr-3 ' + styles.dots}>
                 <ul>
                   {[0, 1, 2].map(i => (
                     <li key={i}>
-                      <a
-                        onClick={() => handleDealChange(i)}
-                        className={i === deal ? styles.active : ''}
-                      >
-                        view {i}
-                      </a>
+                      <a className={i === deal ? styles.active : ''}>view {i}</a>
                     </li>
                   ))}
                 </ul>
               </div>
             </div>
-            {promotionalProducts.map((item, index) => (
-              <div key={item.id} className={index !== deal && 'd-none'}>
+            {promotionalProducts.map(item => (
+              <div key={item.id}>
                 <PromotionalProduct {...item} />
               </div>
             ))}
           </div>
-          <div className='col-7'></div>
+          <div className='col-8'>
+            <BestDeal />
+          </div>
         </div>
       </div>
     </div>
