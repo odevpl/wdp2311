76c68f073b024171182649eae1d5dd96f9509aa8
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-11-14 14:16:34 +0000

    Compare Section - development

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -11,7 +11,17 @@ import { addProductToCompare } from '../../../redux/compareRedux';
 import { useDispatch } from 'react-redux';
 import Popup from '../Popup/Popup';
 
-const ProductBox = ({ name, price, promo, stars, isFavorite, isCompare, id, ownRating, oldPrice  }) => {
+const ProductBox = ({
+  name,
+  price,
+  promo,
+  stars,
+  isFavorite,
+  isCompare,
+  id,
+  ownRating,
+  oldPrice,
+}) => {
   const buttonFavoriteActive = clsx('outline', {
     [styles.favorite]: isFavorite,
   });
@@ -24,7 +34,7 @@ const ProductBox = ({ name, price, promo, stars, isFavorite, isCompare, id, ownR
   const [backgroundBlur, setBackgroundBlur] = useState(false);
 
   const dispatch = useDispatch();
-  const product = { name };
+  const product = { name, price };
 
   const addToCompare = () => {
     dispatch(addProductToCompare(product));
