493d6a3a5bc7a5e835b049617429ea373dfa5b8b
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-11-15 00:29:23 +0300

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
@@ -15,11 +15,11 @@ const App = () => (
   <Provider store={store}>
     <BrowserRouter>
       <MainLayout>
-        <Routes>
-          <Route path={'/'} element={<Homepage />} />
-          <Route path={'/shop/:categoryId'} element={<ProductList />} />
-          <Route path={'/product/:productId'} element={<ProductPage />} />
-        </Routes>
+        <Switch>
+          <Route exact path={'/'} component={Homepage} />
+          <Route exact path={'/shop/:categoryId'} component={ProductList} />
+          <Route exact path={'/product/:productId'} component={ProductPage} />
+        </Switch>
       </MainLayout>
     </BrowserRouter>
   </Provider>
