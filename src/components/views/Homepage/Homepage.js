83f8fd0bba0989c6f42a00b2bc7911d67ee96585
Dominik Wilk <mr.wilkd@gmail.com>
2023-11-16 12:28:46 +0000

    Remove double import

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -4,17 +4,10 @@ import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import ClientFeedback from '../ClientFeedback/ClientFeedback';
-
 import BrandsSlider from '../Brands/Brands';
 import Blog from '../Blog/Blog';
-
 import Promotional from '../../features/Promotional/Promotional';
-
 import Chatbot from '../../views/Chatbot/Chatbot';
-
-
-import BrandsSlider from '../Brands/Brands';
-
 import Gallery from '../../layout/Gallery/Gallery';
 import SectionBlog from '../SectionBlog/SectionBlog';
 import Deals from '../../features/Deals/Deals';
