2304b0d12c23b2e163faba52828d6663aff005fa
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-11-17 15:25:16 +0100

    Add RWD for Deals

--- src/components/features/Deals/Deals.js
+++ src/components/features/Deals/Deals.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
 import { useSelector } from 'react-redux';
 import DealBox from './DealBox';
 import RightBox from './RightBox';
+import styles from './Deals.module.scss';
 
 const Deals = () => {
   const dealsData = useSelector(state => state.promotions);
 
   return (
-    <div className='container'>
+    <div className={`container ${styles.dealsContainer}`}>
       <div className='row'>
         <div className='col-md-6'>
           <DealBox key={dealsData[0].id} {...dealsData[0]} />
@@ -18,14 +18,15 @@ const Deals = () => {
             {dealsData
               .slice(1, 3)
               .map(({ id, title, description, imgSrc, subtitle }) => (
-                <RightBox
-                  key={id}
-                  id={id}
-                  title={title}
-                  subtitle={subtitle}
-                  description={description}
-                  imgSrc={imgSrc}
-                />
+                <div key={id} className='col-md-6'>
+                  <RightBox
+                    id={id}
+                    title={title}
+                    subtitle={subtitle}
+                    description={description}
+                    imgSrc={imgSrc}
+                  />
+                </div>
               ))}
           </div>
         </div>
