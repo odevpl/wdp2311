5177b225bafec0ac37e2280eb5ea196373f96d9b
Dominik Wilk <mr.wilkd@gmail.com>
2023-11-19 21:37:25 +0000

    Fix Deals components & style for cart icon and search input

--- src/components/features/Deals/Deals.js
+++ src/components/features/Deals/Deals.js
@@ -1,35 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import DealBox from './DealBox';
-import RightBox from './RightBox';
+import RightBoxTop from './RightBoxTop';
+import RightBoxDown from './RightBoxDown';
 import styles from './Deals.module.scss';
 import { Container } from 'react-bootstrap';
 
 const Deals = () => {
   const dealsData = useSelector(state => state.promotions);
-
   return (
     <Container>
       <div className={`container ${styles.dealsContainer}`}>
         <div className='row'>
           <div className='col-md-6'>
-            <DealBox key={dealsData[0].id} {...dealsData[0]} />
+            <DealBox {...dealsData[0]} />
           </div>
           <div className='col-md-6'>
             <div className='row'>
-              {dealsData
-                .slice(1, 3)
-                .map(({ id, title, description, imgSrc, subtitle }) => (
-                  <div key={id} style={{ width: '100%' }}>
-                    <RightBox
-                      id={id}
-                      title={title}
-                      subtitle={subtitle}
-                      description={description}
-                      imgSrc={imgSrc}
-                    />
-                  </div>
-                ))}
+              <RightBoxTop {...dealsData[1]} />
+              <RightBoxDown {...dealsData[2]} />
             </div>
           </div>
         </div>
