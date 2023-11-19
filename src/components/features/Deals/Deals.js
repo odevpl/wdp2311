c6a892fd1152a802f70601fa7373cf67e9dbba37
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-11-19 12:35:14 +0000

    stretch div's to 100% of container

--- src/components/features/Deals/Deals.js
+++ src/components/features/Deals/Deals.js
@@ -3,35 +3,38 @@ import { useSelector } from 'react-redux';
 import DealBox from './DealBox';
 import RightBox from './RightBox';
 import styles from './Deals.module.scss';
+import { Container } from 'react-bootstrap';
 
 const Deals = () => {
   const dealsData = useSelector(state => state.promotions);
 
   return (
-    <div className={`container ${styles.dealsContainer}`}>
-      <div className='row'>
-        <div className='col-md-6'>
-          <DealBox key={dealsData[0].id} {...dealsData[0]} />
-        </div>
-        <div className='col-md-6'>
-          <div className='row'>
-            {dealsData
-              .slice(1, 3)
-              .map(({ id, title, description, imgSrc, subtitle }) => (
-                <div key={id}>
-                  <RightBox
-                    id={id}
-                    title={title}
-                    subtitle={subtitle}
-                    description={description}
-                    imgSrc={imgSrc}
-                  />
-                </div>
-              ))}
+    <Container>
+      <div className={`container ${styles.dealsContainer}`}>
+        <div className='row'>
+          <div className='col-md-6'>
+            <DealBox key={dealsData[0].id} {...dealsData[0]} />
+          </div>
+          <div className='col-md-6'>
+            <div className='row'>
+              {dealsData
+                .slice(1, 3)
+                .map(({ id, title, description, imgSrc, subtitle }) => (
+                  <div key={id} style={{ width: '100%' }}>
+                    <RightBox
+                      id={id}
+                      title={title}
+                      subtitle={subtitle}
+                      description={description}
+                      imgSrc={imgSrc}
+                    />
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
