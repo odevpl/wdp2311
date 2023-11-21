4ff94c204121c1d109c590c02c3ff70370844152
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-11-21 20:21:28 +0100

    Add RWD for BrandsSlider

--- src/components/features/BrandsSlider/BrandsSlider.js
+++ src/components/features/BrandsSlider/BrandsSlider.js
@@ -1,15 +1,16 @@
-import styles from './BrandsSlider.module.scss';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { getLayout } from '../../../redux/layoutRedux';
 import getBrandsImages from './GetBrandsImages';
+import styles from './BrandsSlider.module.scss';
 
 const BrandsSlider = () => {
   const brands = useSelector(state => state.brands);
   const images = getBrandsImages(brands, 9);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [visibleSlides, setVisibleSlides] = useState([]);
 
   const layout = useSelector(state => getLayout(state));
   const slidesLength = images.length;
@@ -19,8 +20,6 @@ const BrandsSlider = () => {
     TABLET: 4,
     MOBILE: 3,
   };
-
-  const slidesToShow = Math.min(slidesPerPage[layout], slidesLength);
 
   const handleNext = () => {
     const nextIndex = (currentIndex + slidesPerPage[layout]) % slidesLength;
@@ -33,19 +32,44 @@ const BrandsSlider = () => {
     setCurrentIndex(prevIndex);
   };
 
-  const visibleSlides = images.slice(currentIndex, currentIndex + slidesToShow);
+  useEffect(() => {
+    const updateVisibleSlides = () => {
+      const newVisibleSlides = images.slice(
+        currentIndex,
+        currentIndex + slidesPerPage[layout]
+      );
+      setVisibleSlides(newVisibleSlides);
+    };
+
+    updateVisibleSlides();
+
+    window.addEventListener('resize', updateVisibleSlides);
+
+    return () => {
+      window.removeEventListener('resize', updateVisibleSlides);
+    };
+  }, [currentIndex, slidesPerPage, layout, images]);
 
   return (
-    <div className={styles.slider}>
-      <button onClick={handleNext}>
+    <div className={`container ${styles.slider}`}>
+      <button onClick={handlePrev} className={`btn ${styles.controlButton}`}>
         <FontAwesomeIcon icon={faChevronLeft} className={styles.fa} />
       </button>
-      {visibleSlides.map(image => (
-        <div key={image} className={styles.item}>
-          <img src={image} alt={`Brand ${image.id}`} />
-        </div>
-      ))}
-      <button onClick={handlePrev}>
+      <div className={`d-flex flex-wrap ${styles.slideContainer}`}>
+        {visibleSlides.map((image, index) => (
+          <div
+            key={index}
+            className={`col lg-12 col md-4 col mb-3 {12 / slidesPerPage[layout]} ${styles.item}`}
+          >
+            <img
+              src={image}
+              alt={`Brand ${index}`}
+              className={`img-fluid ${styles.image}`}
+            />
+          </div>
+        ))}
+      </div>
+      <button onClick={handleNext} className={`btn ${styles.controlButton}`}>
         <FontAwesomeIcon icon={faChevronRight} className={styles.fa} />
       </button>
     </div>
