19fc8d929b9f1d4a36c413ac7d68a013378fb273
Olha Ovcharenko <ovcharenko.olga.2014@gmail.com>
2023-11-23 22:35:42 +0300

    add RWD for Gallery

--- src/components/layout/Gallery/Gallery.js
+++ src/components/layout/Gallery/Gallery.js
@@ -4,19 +4,27 @@ import styles from './Gallery.module.scss';
 import TabsBox from '../../common/TabsBox/TabsBox';
 import ProductsBrowser from '../../features/ProductsBrowser/ProductsBrowser';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getLayout } from '../../../redux/layoutRedux';
 
 function Gallery() {
   const [tabActive, setTabActive] = useState('featured');
-
   const handleTabChange = newTab => {
     setTabActive(newTab);
   };
 
+  const layout = useSelector(getLayout);
+  console.log(layout, 'layout');
+
   return (
     <div className='container my-3'>
-      <div className={styles.wrapper}>
+      <div
+        className={`${styles.wrapper} ${
+          layout === 'MOBILE' ? styles.mobileLayout : ''
+        }`}
+      >
         <div
-          className={styles.leftColumn + ' d-flex flex-column'}
+          className={` ${styles.leftColumn} d-flex flex-column col-sm-12`}
           style={{ gap: '1rem' }}
         >
           <Heading>Furniture gallery</Heading>
@@ -28,7 +36,12 @@ function Gallery() {
             <ProductsBrowser tabActive={tabActive} />
           </TabsBox>
         </div>
-        <div className={styles.rightColumn + ' w-100'}>
+
+        <div
+          className={`${styles.rightColumn} w-100 ${
+            layout === 'MOBILE' ? 'd-none' : 'd-sm-block'
+          }`}
+        >
           <img
             src={window.location.origin + '/images/beds/Aenean Ru Bristique 13.jpg'}
             alt={'image'}
